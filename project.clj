06f63fd93466de6dd5d1223b9e09b49d9b0a0c6a
Brandon Bloom <brandon@brandonbloom.name>
2012-12-09 23:58:14 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dispatch-map "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Persistent map with Clojure's multimethod dispatch semantics"
+  :url "https://github.com/brandonbloom/dispatch-map/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
